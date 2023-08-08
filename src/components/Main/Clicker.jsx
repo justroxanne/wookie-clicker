bd45b31a3433438fe278f24387bd407a4b77eb2d
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-08-08 14:23:54 +0200

    refactorization

--- src/components/Main/Clicker.jsx
+++ src/components/Main/Clicker.jsx
@@ -1,34 +1,38 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useRef, useState, useEffect } from 'react';
 import { CountContext } from '../../utils/Context';
+import AddedPoint from './AddedPoint';
 import Chewie from '../../assets/Images/chewie.png';
 import './Clicker.css';
 
 const Clicker = () => {
   const clicker = useRef(null);
+  const [floatingPoints, setFloatingPoints] = useState([]);
 
-  const { moreClick, countPower, manualClick, moreCredits } =
-    useContext(CountContext);
+  const { moreClick, manualClick, moreCredits } = useContext(CountContext);
 
   const handleClick = (event) => {
     moreClick();
     moreCredits();
     manualClick();
-
-    const point = document.createElement('div');
-    point.className = 'floating-point';
-    point.innerText = `+${countPower}`;
-    // point.style.left = event.clientX + 'px';
-    // point.style.top = event.clientY + 'px';
-
-    document.body.appendChild(point);
-
-    setTimeout(() => {
-      document.body.removeChild(point);
-    }, 3000);
+    setFloatingPoints([
+      ...floatingPoints,
+      <AddedPoint key={Date.now() + Math.random()} />,
+    ]);
   };
+
+  useEffect(() => {
+    if (floatingPoints.length > 0) {
+      const timeoutId = setTimeout(() => {
+        setFloatingPoints([]);
+      }, 3000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [floatingPoints]);
 
   return (
     <div className='clicker-wookie' ref={clicker}>
+      {floatingPoints}
       <img
         className='chewie'
         src={Chewie}
